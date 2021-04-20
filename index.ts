c5e3bc5384f96a0775c416a0cb249b4cb1c9bc27
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-04-20 20:33:52 +0200

    Add: connection to mongoDB and user creation with validation of request data

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
 import path from "path";
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import users from "./routes/users";
 
 const app = express();
 
+const uri =
+  "mongodb+srv://gep:KajTuPise4@cluster-shmeazy.x5g2o.mongodb.net/shmeazy?retryWrites=true&w=majority";
+
+mongoose.set("useCreateIndex", true);
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB...");
+});
+
+app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
+app.use("/api/users", users);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -11,4 +29,23 @@ app.get("/", (req, res) => {
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
+});
+
+const shmeazySchema = new mongoose.Schema({
+  shopping_lists: [
+    { name: String, items: [{ name: String, quantity: String, note: String }] },
+  ],
+});
+
+const Shmeazy = mongoose.model("Shmeazy", shmeazySchema, "shmeazy");
+
+async function getShopingLists() {
+  const shmeazy = await Shmeazy.find();
+  return shmeazy;
+}
+
+app.get("/shoppingLists", async (req, res) => {
+  const lists = await getShopingLists();
+  res.send(lists);
+  console.log(lists);
 });
---
+++ routes/users.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import express from "express";
+import User from "../models/user";
+import Joi from "joi";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validateUser(req.body);
+  console.log(error);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  user = await user.save();
+  res.send(user);
+});
+
+function validateUser(user) {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(1024).required(),
+  });
+
+  return schema.validate(user);
+}
+
+export default router;
