a289b93614e7c1181912c078366a4b67f5c3481f
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-10-05 08:56:03 +0200

    Chore: App first tries to look for PORT set in environmental variable before using default port.

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
