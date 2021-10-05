24de182693dbfb9dee5c83681197d9cbba684ce6
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-10-05 09:13:18 +0200

    Chore: Add Procfile for Heroku.

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
